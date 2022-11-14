f84788926d215c73495ff355dc98198bea1cc552
Noviar Putra Budiawan <45009698+NoviarPutra@users.noreply.github.com>
2022-11-14 18:11:19 +0700

    fixing

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -5,40 +5,41 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
-    Scanner userInputScanner = new Scanner(System.in);
-    int row;
-    System.out.print("1=Square, 2=Triangle, 3=MirrorTriangle, 4=Pyramid : ");
-    int opt = userInputScanner.nextInt();
-    switch (opt) {
-      case 1:
-        System.out.print("Masukan jumlah row untuk mencetak SQUARE : ");
-        row = userInputScanner.nextInt();
-        System.out.println("========== OUTPUT ==========");
-        squarePattern(row);
-        break;
-      case 2:
-        System.out.print("Masukan jumlah row untuk mencetak TRIANGLE : ");
-        row = userInputScanner.nextInt();
-        System.out.println("========== OUTPUT ==========");
-        trianglePatter(row);
-        break;
-      case 3:
-        System.out.print(
-          "Masukan jumlah row untuk mencetak MIRROR TRIANGLE : "
-        );
-        row = userInputScanner.nextInt();
-        System.out.println("========== OUTPUT ==========");
-        triangleReversePattern(row);
-        break;
-      case 4:
-        System.out.print("Masukan jumlah row untuk mencetak PYRAMID : ");
-        row = userInputScanner.nextInt();
-        System.out.println("========== OUTPUT ==========");
-        pyramidPattern(row);
-        break;
-      default:
-        System.out.println("Error");
-        break;
+    try (Scanner userInputScanner = new Scanner(System.in)) {
+      int row;
+      System.out.print("1=Square, 2=Triangle, 3=MirrorTriangle, 4=Pyramid : ");
+      int opt = userInputScanner.nextInt();
+      switch (opt) {
+        case 1:
+          System.out.print("Masukan jumlah row untuk mencetak SQUARE : ");
+          row = userInputScanner.nextInt();
+          System.out.println("========== OUTPUT ==========");
+          squarePattern(row);
+          break;
+        case 2:
+          System.out.print("Masukan jumlah row untuk mencetak TRIANGLE : ");
+          row = userInputScanner.nextInt();
+          System.out.println("========== OUTPUT ==========");
+          trianglePatter(row);
+          break;
+        case 3:
+          System.out.print(
+            "Masukan jumlah row untuk mencetak MIRROR TRIANGLE : "
+          );
+          row = userInputScanner.nextInt();
+          System.out.println("========== OUTPUT ==========");
+          triangleReversePattern(row);
+          break;
+        case 4:
+          System.out.print("Masukan jumlah row untuk mencetak PYRAMID : ");
+          row = userInputScanner.nextInt();
+          System.out.println("========== OUTPUT ==========");
+          pyramidPattern(row);
+          break;
+        default:
+          System.out.println("Error");
+          break;
+      }
     }
     System.out.print("- END PROGRAM -");
   }
